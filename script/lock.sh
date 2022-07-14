d07291cb341853d11ab6298bb9ce426544c219c7
Takane42 <dragster1616@gmail.com>
2022-07-14 08:13:10 +0700

    new config

---
+++ script/lock.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BLANK='#00000030'
+CLEAR='#ffffff22'
+DEFAULT='#1fd655cc'
+TEXT='#00bcd4FF'
+WRONG='#880000bb'
+VERIFYING='#00ab41bb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 10                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
